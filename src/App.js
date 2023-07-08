70ecf2ae9b50827743a6efd1f6c126ea75ab3a0c
swapnaliss <swapnalisbhargude123@gmail.com>
2023-07-08 19:43:07 +0530

    Displayed a list of restaurants based on the selected cuisine.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import CuisinesList from "./components/CuisinesList"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const cuisineData = [
-  { id: 1, name: "Italian", reviews: [], averageRating: 4 },
-  { id: 2, name: "Mexican", reviews: [], averageRating: 4 },
-  { id: 3, name: "Chinese", reviews: [], averageRating: 4 },
-  { id: 4, name: "Indian", reviews: [], averageRating: 4 }
-];
 
 function App() {
   
   return (
     <div className="App">
        <CuisinesList/>
+      
     </div>
   );
 }
--- src/components/CuisinesList.jsx
+++ src/components/CuisinesList.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import {Container , Button } from "react-bootstrap";
-import {cuisineData} from "../App"
+import React, { useState } from 'react';
+import {  Card, Row, Col,Button } from 'react-bootstrap';
+import { cuisineData, restaurantsData } from './Data';
+
 const CuisinesList = () => {
-  return (
-    <div> 
-    <Container>
-    <h1>List of Cuisines</h1>
-    <div className="d-flex flex-wrap">
-      {cuisineData.map((cuisine) => (
-        <Button key={cuisine.id} variant="primary" className="m-2">
-          {cuisine.name}
-        </Button>
-      ))}
-    </div>
-  </Container> </div>
-  )
+
+
+    const [selectedCuisine, setSelectedCuisine] = useState(null);
+
+    const handleCuisineClick = (cuisine) => {
+        setSelectedCuisine(cuisine);
+    };
+
+    const filteredRestaurants = selectedCuisine
+        ? restaurantsData.filter((restaurant) => restaurant.cuisine_id === selectedCuisine.id)
+        : [];
+
+    return (
+       <div>
+         <h1>List of Cuisines</h1>
+      <div className="d-flex flex-wrap">
+        {cuisineData.map((cuisine) => (
+          <Button
+            key={cuisine.id}
+            variant={selectedCuisine === cuisine ? 'primary' : 'secondary'}
+            className="m-2"
+            onClick={() => handleCuisineClick(cuisine)}
+          >
+            {cuisine.name}
+          </Button>
+        ))}
+      </div>
+        { selectedCuisine && (
+            <div>
+                <h2>Restaurants for {selectedCuisine.name}</h2>
+                <Row>
+                    {filteredRestaurants.map((restaurant) => (
+                        <Col key={restaurant.id} sm={6} md={4} lg={3} className="mb-3">
+                            <Card>
+                                <Card.Img variant="top" src={restaurant.menu[0].imgSrc} />
+                                <Card.Body>
+                                    <Card.Title>{restaurant.name}</Card.Title>
+                                    <Card.Text>Price: {restaurant.menu[0].price}</Card.Text>
+                                    <Card.Text>Food: {restaurant.menu[0].name}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
+        )}
+     </div>
+    )  
 }
 export default CuisinesList;
