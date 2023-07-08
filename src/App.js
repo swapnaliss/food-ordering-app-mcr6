238bbbfb8667e15fe6ec92e46030d383c317957a
swapnaliss <swapnalisbhargude123@gmail.com>
2023-07-08 19:24:14 +0530

    displayed Cuisines List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import CuisinesList from "./components/CuisinesList"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const cuisineData = [
+  { id: 1, name: "Italian", reviews: [], averageRating: 4 },
+  { id: 2, name: "Mexican", reviews: [], averageRating: 4 },
+  { id: 3, name: "Chinese", reviews: [], averageRating: 4 },
+  { id: 4, name: "Indian", reviews: [], averageRating: 4 }
+];
 
 function App() {
+  
   return (
     <div className="App">
        <CuisinesList/>
--- src/components/CuisinesList.jsx
+++ src/components/CuisinesList.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import {Container , Button } from "react-bootstrap";
+import {cuisineData} from "../App"
 const CuisinesList = () => {
   return (
-    <div> Hello world </div>
+    <div> 
+    <Container>
+    <h1>List of Cuisines</h1>
+    <div className="d-flex flex-wrap">
+      {cuisineData.map((cuisine) => (
+        <Button key={cuisine.id} variant="primary" className="m-2">
+          {cuisine.name}
+        </Button>
+      ))}
+    </div>
+  </Container> </div>
   )
 }
 export default CuisinesList;
